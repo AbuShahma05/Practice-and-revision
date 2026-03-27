17f3c4193c8de7cbdff4556cf95a5040583a989d
AbuShahma05 <abushahmasiddiqui880@gmail.com>
2026-03-27 23:12:46 +0530

    Some code changes

--- day10.js
+++ day10.js
@@ -12,19 +12,19 @@
 //  1. Prototypes & Inheritance
 
 function Animal(name) {
-    this.name = name;
+  this.name = name;
 }
-Animal.prototype.speak = function() {
-    return `${this.name} makes a sound.`;
+Animal.prototype.speak = function () {
+  return `${this.name} makes a sound.`;
 };
 
 function Dog(name) {
-    Animal.call(this, name);
+  Animal.call(this, name);
 }
 Dog.prototype = Object.create(Animal.prototype);
 Dog.prototype.constructor = Dog;
-Dog.prototype.speak = function() {
-    return `${this.name} barks.`;
+Dog.prototype.speak = function () {
+  return `${this.name} barks.`;
 };
 
 const dog = new Dog("Tommy");
@@ -33,21 +33,21 @@ console.log(dog.speak()); // Tommy barks.
 //  2. Encapsulation (via Closures)
 
 function Counter() {
-    let count = 0; // private variable
+  let count = 0; // private variable
 
-    return {
-        increment: function() {
-            count++;
-            return count;
-        },
-        decrement: function() {
-            count--;
-            return count;
-        },
-        getCount: function() {
-            return count;
-        }
-    };
+  return {
+    increment: function () {
+      count++;
+      return count;
+    },
+    decrement: function () {
+      count--;
+      return count;
+    },
+    getCount: function () {
+      return count;
+    },
+  };
 }
 
 const counter = Counter();
@@ -55,19 +55,19 @@ console.log(counter.increment()); // 1
 console.log(counter.increment()); // 2
 console.log(counter.decrement()); // 1
 console.log(counter.decrement()); // 0
-console.log(counter.getCount());  // 0
+console.log(counter.getCount()); // 0
 // count is not accessible directly (encapsulation)
 
 //  3. Factory Functions
 
 function createUser(name, age) {
-    return {
-        name,
-        age,
-        greet() {
-            return `Hello, I'm ${name}, ${age} years old.`;
-        }
-    };
+  return {
+    name,
+    age,
+    greet() {
+      return `Hello, I'm ${name}, ${age} years old.`;
+    },
+  };
 }
 
 const user1 = createUser("Abu", 22);
@@ -76,9 +76,9 @@ console.log(user1.greet()); // Hello, I'm Abu, 22 years old.
 //  4. Closures
 
 function outerFunction(outerVar) {
-    return function innerFunction(innerVar) {
-        return `Outer: ${outerVar}, Inner: ${innerVar}`;
-    };
+  return function innerFunction(innerVar) {
+    return `Outer: ${outerVar}, Inner: ${innerVar}`;
+  };
 }
 
 const closureExample = outerFunction("React");
@@ -89,16 +89,16 @@ console.log(closureExample("Hooks")); // Outer: React, Inner: Hooks
 let globalVar = "I'm global";
 
 function outer() {
-    let outerVar = "I'm outer";
+  let outerVar = "I'm outer";
 
-    function inner() {
-        let innerVar = "I'm inner";
-        console.log(globalVar); 
-        console.log(outerVar);  
-        console.log(innerVar);  
-    }
+  function inner() {
+    let innerVar = "I'm inner";
+    console.log(globalVar);
+    console.log(outerVar);
+    console.log(innerVar);
+  }
 
-    inner();
+  inner();
 }
 
 outer();
@@ -108,4 +108,3 @@ outer();
 Lexical Scope = where the variable is written (physically)
 Closure = when a function remembers variables from its lexical scope
 */
-
